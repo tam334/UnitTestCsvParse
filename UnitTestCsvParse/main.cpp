d2907e9927b64ccc8da129ebfcbc8221312ad576
y_tsukuda <y.tsukuda.gm.01@gmail.com>
2023-09-26 17:51:32 +0900

    ファイル読み込みをストリームに変更、内容を全て読む

--- UnitTestCsvParse/main.cpp
+++ UnitTestCsvParse/main.cpp
@@ -6,23 +6,30 @@
 //
 
 #include <iostream>
+#include <fstream>
 #include <list>
 #include "CsvParse.h"
 
 int main(int argc, const char * argv[]) {
     
-    FILE* fp = fopen("家計簿.csv", "r");
-    if(fp != NULL)
+    std::ifstream ifs("家計簿.csv");
+    if(ifs)
     {
-        fclose(fp);
+        std::string text("");
+        while(ifs)
+        {
+            std::string line;
+            std::getline(ifs, line);
+            text += line;
+        }
+        std::list<std::list<std::string>> csv;
+        Parse(csv, text);
     }
     else
     {
         printf("file not exist 家計簿.csv");
     }
     
-    std::list<std::list<std::string>> csv;
-    std::string text("");
-    Parse(csv, text);
+    
     return 0;
 }
