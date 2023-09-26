0dbc792910d4400129d52927a8d98f18c70c898c
y_tsukuda <y.tsukuda.gm.01@gmail.com>
2023-09-26 17:33:23 +0900

    ファイルの読み込みを行うように xcodeのワーキングディレクトリも指定してファイルのある場所にする

--- UnitTestCsvParse/main.cpp
+++ UnitTestCsvParse/main.cpp
@@ -10,6 +10,17 @@
 #include "CsvParse.h"
 
 int main(int argc, const char * argv[]) {
+    
+    FILE* fp = fopen("家計簿.csv", "r");
+    if(fp != NULL)
+    {
+        fclose(fp);
+    }
+    else
+    {
+        printf("file not exist 家計簿.csv");
+    }
+    
     std::list<std::list<std::string>> csv;
     std::string text("");
     Parse(csv, text);
